1df821ef73161714fdf6f1dd012405ab6ccef7a1
Tatiana-goit <tanchou74@gmail.com>
2021-11-18 09:26:42 +0200

    done all work

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,15 +4,3 @@ import contactReducer from './Phonebook/phone-reducers';
 
 const store = createStore(contactReducer, composeWithDevTools());
 export default store;
-
-
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import contactReducer from './Phonebook/phone-reducers';
-
-// const store = configureStore({
-//   reducer: contactReducer,
-//   devTools: process.env.NODE_ENV !== 'production',
-// });
-
-// export default store;
